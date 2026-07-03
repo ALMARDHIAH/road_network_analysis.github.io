b8b40c2d898e4000ee0612ac44a9e245b5a73c51
almardhiah <ainulmardhiah0488@gmail.com>
2026-07-03 23:36:56 +0800

    Update degree_centrality.py

--- degree_centrality.py
+++ degree_centrality.py
@@ -2,28 +2,28 @@ import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Downloading geospatial data from OSM
+# Memuat turun data georuang daripada OSM
 graf = ox.graph_from_place(
 “putrajaya”,
 network_type='drive',
 simplify=True)
 
-# Projecting the graph based on UTM
+# Mengunjurkan graf berdasarkan UTM
 graf_proj=ox.project_graph(graf)
 
-# Downloading GeoDataFrame of nodes and edges
+# Memuat turun GeoDataFrame bucu dan tepi rangkaian
 bucu, tepi= ox.graph_to_gdfs(graf_proj)
 
-# Identifying the degree of each node
+# Mendapatkan darjah
 darjah=nx.degree(graf_proj)
 
-# Inserting ‘degree’ row into the node GeoDataFrame
+# Memasukkan lajur ‘darjah’ ke dalam GeoDataFrame bucu
 bucu['darjah']=bucu.index.map(darjah)
 
-# Showing ‘darjah’ in descending order
+# Memaparkan ‘darjah’ mengikut tertib menurun
 print(bucu[['darjah']].sort_values(by='darjah',ascending=False))
 
-# Plotting the degree of each node in the network
+# Memplot darjah bagi setiap bucu dalam rangkaian
 figure, ax = plt.subplots(figsize=(8.5,8))
 bucu.plot(ax=ax, markersize=1, 
           column='darjah', 
